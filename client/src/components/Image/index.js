2f42a758af5563215c06e49ac18639cc29c54c1d
tlockhart <tony.lockhart@ymail.com>
2019-11-01 00:56:47 -0400

    fixed image dimensions on productInsertForm

--- client/src/components/Image/index.js
+++ client/src/components/Image/index.js
@@ -1,21 +1,15 @@
 import React, { Component } from 'react';
 import { MDBBtn } from "mdbreact";
 import $ from 'jquery';
+import styles from './styles.css';
 
 export class Uploader extends Component {
   // render() {
   constructor(props) {
     super(props);
-    // FacePlusPlus API:
-    this.key = 'o5qW4trQcuO1e8ElIJEIDnecHNILHOSu';
-    this.secret = 'i4yY2sL2dg8cOxpUYTN8AZ4pKMHGP2lV';
 
+    // the input file
     this.input = null;
-
-    // When there is a change on the input field call updateImageDisplay
-    // this.input.addEventListener('change', this.updateImageDisplay);
-
-    // this.preview = null;
 
     this.fileTypes = [
       'image/jpeg',
@@ -28,43 +22,54 @@ export class Uploader extends Component {
     this.list = null;
     this.listItem = null;
     this.br = document.createElement('br');
-    // var br = document.createElement('BR');
     this.para = document.createElement('p');
-    this.preview = '';
+    // canvas div
+    // this.preview = document.querySelector('.preview');
+    // this.preview = '';
     this.files = [];
-    this.fileName = '';
-    this.fileSize = 0;
+    this.imageName = '';
+    // this.fileName = '';
+    // this.fileSize = 0;
     this.image = null;
+    // Image Dimensions
     this.imageWidth = 0;
     this.imageHeight = 0;
-    this.isInputValid = false
-    this.faceToken = null
+    this.imageSize = 0;
+    this.imageSrc = '';
 
-    // const
+    // const image dimension size
     this.imageMin = 200
     this.imageMax = 800
+
+    // const image file size
     this.maxKB = 2
 
     $('#select-btn').on('click', function () {
       // select button isn't a button, its a label
       // event.preventDefault();
-      $('#select-btn').hide()
+      $('#select-btn').hide();
 
       // enable submit-image (Upload Image Button)
-      $('#submit-image').prop('disabled', false)
+      $('#submit-image').prop('disabled', false);
     });
   }
 
   componentDidMount() {
     // this.input = document.querySelector('#image-input');
     // this.preview = document.querySelector('.preview');
+    this.preview = document.querySelector('.preview');
   }
 
-  imgOnLoad = () => {
-    this.imageWidth = this.width
-    this.imageHeight = this.height
+  // imgOnLoad2 = (img) => {console.log(img)};
+  displayImageProps = () => {
+    // img.onload = () => {
+    // console.log('IMGONLOAD_IMG', img);
+    // console.log('IMGONLOAD_IMG.WIDTH', img.width);
+    // }
+    // this.imageWidth = this.width
+    // this.imageHeight = this.height
 
-    this.para = document.createTextNode('File Name: ' + this.fileName + ', File Size: ' + this.fileName + ', Width: ' + this.imageWidth + ', Height: ' + this.imageHeight);
+    this.para = document.createTextNode('File Name: ' + this.imageName + ', File Size: ' + this.imageSize + ', Width: ' + this.imageWidth + ', Height: ' + this.imageHeight);
 
     this.listItem = document.createElement('li');
     this.listItem.setAttribute('id', 'li-id');
@@ -72,17 +77,20 @@ export class Uploader extends Component {
     this.listItem.appendChild(this.br);
     this.listItem.appendChild(this.para);
 
-    this.list = document.createElement('ol')
-    this.preview.appendChild(this.list)
+    this.list = document.createElement('ol');
+    this.preview.appendChild(this.list);
 
     this.list.appendChild(this.listItem);
 
     // Insert break before paragraph:
     this.listItem.insertBefore(this.br, this.para);
-    if (this.imageWidth >= this.imageMin && this.imageWidth <= this.imageMax && this.imageHeight >= this.imageMin && this.imageHeight <= this.imageMax) { } else {
+    if (this.imageWidth >= this.imageMin && this.imageWidth <= this.imageMax && this.imageHeight >= this.imageMin && this.imageHeight <= this.imageMax) {
+      console.log("IMAGE IS GOOD");
+      document.getElementById('submit-image').disabled = false
+    } else {
       this.para = document.createTextNode(
-        'File Name: ' + this.fileName,
-        'File Size: ' + this.fileName,
+        'File Name: ' + this.imageName,
+        'File Size: ' + this.imageSize,
         'Width: ' + this.imageWidth,
         'Height: ' + this.imageHeight
       )
@@ -91,19 +99,21 @@ export class Uploader extends Component {
       // document.getElementById('submit-image').disabled = true
       $('#li-id').css({ 'color': 'red' })
     }
+    console.log("in imgONLoad")
   };
 
   imgOnError = () => {
-    // if ( imageWidth <= imageMax && imageHeight <= imageMax )
-    // {
-    // console.log('NOT A Valid File: '+file.type);
-    var error = document.createElement('p')
-    var node = document.createTextNode('Not a valid file: ' + this.file.type)
-    error.appendChild(node)
-    this.preview.appendChild(error)
-    // document.getElementById('submit-image').disabled = true
-    $('#select-btn').show()
-    // }
+    if (this.imageWidth <= this.imageMax && this.imageHeight <= this.imageMax) {
+      console.log('NOT A Valid File: ' + this.file.type);
+      var error = document.createElement('p')
+      var node = document.createTextNode('Not a valid file: ' + this.file.type)
+      error.appendChild(node)
+      this.preview.appendChild(error)
+      // document.getElementById('submit-image').disabled = true
+      $('#select-btn').show()
+      console.log("IMAGE ERROR ONLOAD")
+    }
+    console.log("in imgONERROR")
   }; // oneerror
 
   // Returns formatted version of file size
@@ -125,15 +135,15 @@ export class Uploader extends Component {
         return true
       }
     }
-
     return false
   }
 
+  // When there is a change on the input field call
   updateImageDisplay = () => {
     // event.preventDefault();
 
     // Set Dimensions
-    // this.stageImage();
+    // this.selectImage();
     // empty previous contents of .preview
     while (this.preview.firstChild) {
       this.preview.removeChild(this.preview.firstChild);
@@ -149,24 +159,27 @@ export class Uploader extends Component {
       this.para.textContent = 'No file currently selected for upload'
       this.preview.appendChild(this.para);
     } else {
-      this.list = document.createElement('ol');
+      this.list = document.createElement('ul');
       this.preview.appendChild(this.list);
       for (var i = 0; i < curFiles.length; i++) {
         this.listItem = document.createElement('li');
         this.listItem.setAttribute('id', 'li-id');
         // Print the image name and file size if the file type matches the accepted types
         if (this.isFileTypeValid(curFiles[i])) {
-          this.fileName = curFiles[i].name;
+          // this.fileName = curFiles[i].name;
           this.fileSize = this.returnFileSize(curFiles[i].size);
           // console.log(curFiles[i]);
           this.image = document.createElement('img');
           this.image.setAttribute('id', 'image-id');
-          this.image.src = window.URL.createObjectURL(curFiles[i]);
-          this.image.style.width = '200px';
+          // this.image.src = window.URL.createObjectURL(curFiles[i]);
+          this.image.src = this.imageSrc;
+          // this.image.style.width = '200px';
+          this.image.style.width = this.imageWidth;
           this.listItem.appendChild(this.image);
           // document.getElementById('submit-image').disabled = false;
         } else {
-          this.para.textContent = 'File name ' + curFiles[i].name + ': File type not valid. Please select an image.';
+          // this.para.textContent = 'File name ' + curFiles[i].name + ': File type not valid. Please select an image.';
+          this.para.textContent = 'File name ' + this.imageName + ': File type not valid. Please select an image.';
           this.listItem.appendChild(this.para);
           // document.getElementById('submit-image').disabled = true;
         }// else
@@ -191,8 +204,8 @@ export class Uploader extends Component {
     img.onload = () => {
 
       canvas.setAttribute("id", `canvas${this.count}`);
-      console.log("IMG:", img);
-      console.log("IMG Width:", img.width, "IMG HT:", img.height);
+      console.log("APPEND_IMG:", img);
+      console.log("APPEND_IMG Width:", img.width, "IMG HT:", img.height);
 
       // Set canvas dimension to match image
       canvas.width = img.width
@@ -203,7 +216,7 @@ export class Uploader extends Component {
 
       // Draw image to canvas
       context.drawImage(img, 0, 0);
-      this.preview = document.querySelector('.preview');
+      // this.preview = document.querySelector('.preview');
 
       // Apend image to canvas div
 
@@ -228,29 +241,76 @@ export class Uploader extends Component {
       this.appendChild(img, canvas);
     }
   };
+
+  loadImage = (img, blob) => {
+    return new Promise(function (resolve, reject) {
+      // define source
+      img.src = URL.createObjectURL(blob);
+      // resolve promise onLoad
+      img.onload = () => {
+        console.log('LOADIMAGE_IMG.WIDTH', img.width);
+        // this.imageWidth = img.width;
+        console.log('LOADIMAGE_IMG.HEIGHT', img.height);
+        // this.imageHeight = img.height;
+        console.log('LOADIMAGE_BLOB.SIZE', blob.size);
+        // this.imageSize = blob.size;
+        console.log('LOADIMAGE_BLOB.Name', blob.name);
+
+        const imageProps = {
+          imageName: blob.name,
+          imageHeight: img.height,
+          imageWidth: img.width,
+          imageSize: blob.size,
+          imageSrc: img.src
+        }
+        resolve(imageProps);
+      }
+      // reject promis onError
+      img.onerror = () => {
+        reject("rejected")
+      }
+    })
+  }
+
   //  Select an image
-  stageImage = (event) => {
+  selectImage = async (event) => {
     event.preventDefault();
 
     // Set reference to #image-input div
     this.getFileInfo();
 
     var _URL = window.URL || window.webkitURL;
-    var img;
-    // var file = this.input.files[0];
-    // if ((file = this.files[0])) {
-    img = new Image();
-    // img.onload = this.imgOnLoad();
-    // img.onError = this.imgOnError();
-    img.src = URL.createObjectURL(this.file);
+    let img;
+    var blob = this.input.files[0];
+    if (blob) {
+      img = new Image();      
+      try {
+        const result = await this.loadImage(img, blob);
+        console.log("RESULT", result);
 
-    // }// if
-    console.log("IMAGE NAME", this.file);
+        // Set variables
+        img.src = result.imageSrc;
+        this.imageSrc = result.imageSrc;
+        this.imageWidth = result.imageWidth;
+        this.imageHeight = result.imageHeight;
+        this.imageSize = result.imageSize;
+        this.imageName = result.imageName;
 
-    // Create Canvas and load image
-    this.displayImage(img);
+
+        this.displayImageProps();
+
+        // Create Canvas and load image
+        this.displayImage(img);
+      }
+      catch(err) {
+        console.log("failure ", err);
+        this.imgOnError()
+      }
+      console.log("IMAGE NAME", this.file);
+    }// if
   };
 
+  // Sets message of preview tag
   enableSubmitBtn = () => {
     var $ptag = $('.preview #file-msg').text()
     if ($ptag === 'File not accepted.') {
@@ -259,62 +319,6 @@ export class Uploader extends Component {
       // console.log("Image not processed");
     }
   };
-
-  formComplete = () => {
-    var isFormComplete =
-      $('#name-input').val() &&
-      $('#intel-value').text() &&
-      $('#stren-value').text() &&
-      $('#speed-value').text() &&
-      $('#durab-value').text() &&
-      $('#power-value').text() &&
-      $('#combat-value').text() &&
-      $('#face').text() === 'Identity processed!' &&
-      $('#steps').text() === 'Your selections are locked in.'
-
-    if (isFormComplete) {
-      $('#submit-btn').show()
-      $('#submit-btn').prop('disabled', false)
-      $('#reset-btn').hide()
-    }
-  };
-
-
-  /********************************************
-   * Store Face_Token for data Analysis:
-   * ******************************************/
-  parseToken = (faceObj) => {
-    this.faceToken = faceObj.faces[0].face_token
-    // console.log(faceToken);
-
-    // Call FaceAPI to Analyze data
-    this.analyzeFace()
-  }
-
-  base64Callback = (base64Str) => {
-    var cors = 'https://cors-anywhere.herokuapp.com/'
-    var query = cors + 'https://api-us.faceplusplus.com/facepp/v3/detect'
-
-    $.ajax({
-      url: query,
-      method: 'POST',
-      data: {
-        'api_key': this.key,
-        'api_secret': this.secret,
-        'image_base64': base64Str,
-        'return_landmark': 1,
-        'return_attributes': 'gender,age',
-        // dataType: 'jsonp',
-        // success: options.success,
-        // error: options.error,
-        timeout: 10 * 1000
-      }
-    }).then(function (response) {
-      // Create CODE HERE to Log the queryURL
-      // Create CODE HERE to log the resulting object
-      this.parseToken(response)
-    })
-  };// convertImageFromUrlToBase64String
 
   returnFileSize = (number) => {
     if (number < 1024) {
@@ -326,7 +330,7 @@ export class Uploader extends Component {
     }
   };
 
-  convertImageFromUrlToBase64String = (url, callbackFn) => {
+  convertImageFromUrlToBase64String = (url) => {
     var img = new Image()
     img.crossOrigin = 'Anonymous'
     var dataUrl;
@@ -344,25 +348,12 @@ export class Uploader extends Component {
       // Return a data URI containing a representation of the image in jpg format
       dataUrl = canvas.toDataURL('image/jpg');
       console.log("convertImageFromUrlToBase64String-DataURL:", dataUrl);
-      var isCallBackAFunction = typeof callbackFn === 'function'
-      console.log("isCallBackAFunction:", isCallBackAFunction);
-
-      /********************************************
-       * Send the dataUrl to the backend, or img.src
-       ********************************************
-      if (dataUrl && isCallBackAFunction) {
-        // look for "data: image/png;base64,"
-        // or look for "data: image/jpg;base64,"
-        // or look for "data: image/jpeg;base64,"
-        // The Call back function, passes it the dataURL (base64 image) as an argument
-        callbackFn(dataUrl.replace(/^data:image\/(png|jpg|jpeg);base64,/, ''))
-      }
-      ********************************************/
     }
 
     // Setting the img.src will call img.onload when the src is loaded
     img.src = url// url is the img src
-    console.log("IMG:", img, "Name:", this.fileName);
+    // console.log("IMG:", img, "Name:", this.fileName);
+    console.log("IMG:", img, "Name:", this.imageName);
     console.log("IMG SRC:", url);
     // console.log("BASE64:", dataUrl);
 
@@ -372,38 +363,43 @@ export class Uploader extends Component {
     // Don't refresh the page!
     event.preventDefault()
 
+    // checks image dimension and file size
+    let isInputValid = false
+
     //reset the canvas image to 0, so other images can be selected after submit
     this.count = 0;
 
     // this.getFileInfo();
     this.updateImageDisplay();
 
-    /*********************************************************************
-    *Enablesubmit Button for Error Handling
-    ***********************************************************************/
+    /***************************************
+    *Disable Select Button, and Enable submit Button for Error Handling
+    ****************************************/
     $('#select-btn').attr('disabled', 'disabled')
-    // $('#submit-image').prop('disabled', true)
-    // face++ timer to give results time to return
+
+    // set preview message and enable submitBtn
     this.enableSubmitBtn()
 
     /***********************************************************************/
 
     // Check Image Dimensions
     if (this.imageWidth >= this.imageMin && this.imageWidth <= this.imageMax && this.imageHeight >= this.imageMin && this.imageHeight <= this.imageMax) {
-      this.isInputValid = true
+      isInputValid = true
     } else {
-      this.isInputValid = false
+      isInputValid = false
     }
 
     // Check File Size
-    console.log("FileName:", this.fileName);
-    console.log("FileSize:", this.fileSize);
+    // console.log("FileName:", this.fileName);
+    console.log("FileName:", this.imageName);
+    // console.log("FileSize:", this.fileSize);
+    console.log("FileSize:", this.imageSize);
 
     // Get Unit of Measure
-    var unit = this.fileSize.slice(-2).toLowerCase();
+    var unit = this.imageSize.slice(-2).toLowerCase();
 
     // Get FileSize
-    var fileSizeNumber = this.fileSize.replace(/[^\d.-]/g, '');
+    var fileSizeNumber = this.imageSize.replace(/[^\d.-]/g, '');
 
     console.log("FILESIZE = " + fileSizeNumber + "Unit = " + unit);
     console.log("Max File Size:", this.maxKB);
@@ -412,23 +408,23 @@ export class Uploader extends Component {
     if (unit === 'mb') {
       console.log("max file size:", this.maxKB, ", actual file size:", fileSizeNumber);
       if (fileSizeNumber <= this.maxKB) {
-        this.isInputValid = true
+        isInputValid = true
       } else {
-        this.isInputValid = false
+        isInputValid = false
       }
     }
     else if (unit === 'kb') {
       if (fileSizeNumber <= this.maxKB * 1000) {
-        this.isInputValid = true
+        isInputValid = true
       } else {
-        this.isInputValid = false
+        isInputValid = false
       }
 
     }
 
     // If input is not valid do not accept image and do nothing
-    console.log("Is Input File Valid:", this.isInputValid);
-    if (!this.isInputValid) {
+    console.log("Is Input File Valid:", isInputValid);
+    if (!isInputValid) {
       // console.log ("Image is not acceptable!");
       var fileMsg = document.getElementById('file-msg')
       // console.log ("IS FILEMSG CREATED = "+fileMsg);
@@ -463,7 +459,7 @@ export class Uploader extends Component {
          *base64Str from the callbackFn and sends it to Face++API:
          *It will take 10 sec depending on photo size
         ***********************************************************/
-      this.convertImageFromUrlToBase64String(imageUrl, this.base64Callback);
+      this.convertImageFromUrlToBase64String(imageUrl);
     }// else
   };// submit-Image on click
 
@@ -494,7 +490,7 @@ export class Uploader extends Component {
                 accept=".png, .jpeg, .jpg"
                 onChange={
                   (event) => {
-                    this.stageImage(event)
+                    this.selectImage(event)
                     // this.updateImageDisplay(event)
                   }
                 }
@@ -502,7 +498,7 @@ export class Uploader extends Component {
               {/* UPLOAD IDENTITY BUTTON */}
               <button
                 className="btn btn-info"
-                // disabled ="disabled" 
+                disabled="disabled"
                 id="submit-image"
                 type="submit"
                 onClick={
@@ -518,7 +514,7 @@ export class Uploader extends Component {
               <div className="preview">
                 <h4>Image Preview</h4>
                 <p id="error">No file currently selected for upload</p>
-                <p id="file-msg">File accepted</p>
+                <p id="file-msg"></p>
                 {/* <!--p id="face"></p--> */}
               </div>
             </div>
