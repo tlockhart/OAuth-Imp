602828685c75bb0b1d0f018b094896fb38808cb3
tlockhart <tony.lockhart@ymail.com>
2020-01-18 03:43:44 -0500

    updated content-type in product controller

--- api/controllers/products.js
+++ api/controllers/products.js
@@ -24,11 +24,12 @@ exports.products_get_all = (req, res, next) => {
                     }
                 })
             }; //response
-            res.set('Content-Type', 'application/json');
+            // res.set('Content-Type', 'application/json');
+            res.type('Content-Type', 'application/json');
+
             // res.status(200).json(response);
-            res.status(200);
-            res.send(JSON.stringify(response));
-            console.log("STATUS:", res.status);
+            res.status(200).send(response);
+            console.log("STATUS:", res.statusCode);
             console.log("CONTENT:", res.get('Content-Type'));
         })
         .catch(err => {
--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react';
 import './App.css';
 
 // Handle Routes
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Router, Route, Switch } from "react-router-dom";
 
 // Import Components
 // import Navbar from './components/Navbar';
